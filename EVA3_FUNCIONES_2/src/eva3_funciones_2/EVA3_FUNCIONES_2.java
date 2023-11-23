03c4ef7830cddd3a8fee4a241ed1aa2d284079ff
Saulihio1321 <143568343+Saulihio1321@users.noreply.github.com>
2023-11-22 19:48:22 -0600

    Add files via upload

---
+++ EVA3_FUNCIONES_2/src/eva3_funciones_2/EVA3_FUNCIONES_2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva3_funciones_2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA3_FUNCIONES_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    static int x = 100;
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println(x);
+    }
+    
+}
